0cbe0bece95cff83c41ebca294c56fd5ce5655c9
Annisa Novaguinea <novaguinea.a@gmail.com>
2020-12-28 23:20:59 +0700

    modalnya masi blm ada :(

--- ci_sbd/application/views/Menu/index.php
+++ ci_sbd/application/views/Menu/index.php
@@ -7,9 +7,12 @@
     <div class="row">
 
         <div class="col-lg-12">
+            <a href="" class="btn btn-primary mb-3" data-bs-toggle="modal" data-bs-target="#addProductModal">
+                Add New Product
+            </a>
             <table class="table table-hover">
                 <thead>
-                    <tr>
+                    <tr class="bg-dark text-light">
                         <th scope="col">No.</th>
                         <th scope="col">Kode Obat</th>
                         <th scope="col">Nama Obat</th>
@@ -47,10 +50,52 @@
                 </tbody>
             </table>
         </div>
+
+        <!--card form started-->
+        <div class="card mt-5 w-75">
+            <div class="card-header bg-dark text-light">
+                Drug Input Form
+            </div>
+            <div class="card-body">
+                <form action="" method="post">
+                    <div class="form-group">
+                        <label for="">Kode Obat</label>
+                        <input type="text" name="tkodeobat" maxlength="10" value="<?= @$kodeobat ?>" class="form-control" placeholder="Kode Obat" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Nama Obat</label>
+                        <input type="text" name="tnamaobat" maxlength="20" value="<?= @$namaobat ?>" class="form-control" placeholder="Nama Obat" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Bentuk Obat</label>
+                        <input type="text" name="tbentukobat" maxlength="10" value="<?= @$bentukobat ?>" class="form-control" placeholder="Bentuk Obat" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Tgl Produksi</label>
+                        <input type="date" name="tproduksiobat" value="<?= $tglprod = "" ?>" class="form-control" placeholder="Tgl Produksi Obat" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Tgl Kadaluarsa</label>
+                        <input type="date" name="tkadaluarsaobat" value="<?= $tglexp = "" ?>" class="form-control" placeholder="Tgl Kadaluarsa Obat" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Harga Satuan</label>
+                        <input type="number" name="thargasatuan" maxlength="30" value="<?= $hargasatuan = "" ?>" class="form-control" placeholder="Harga Satuan" required>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary mt-3" name="btnadd">Save</button>
+                    <button type="reset" class="btn btn-secondary mt-3" name="btnreset">Reset</button>
+
+                </form>
+            </div>
+        </div>
+
+        <!--card form ended-->
+
     </div>
 
 </div>
 <!-- /.container-fluid -->
 
-</div>
+
 <!-- End of Main Content -->
