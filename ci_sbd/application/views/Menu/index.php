0a921730b8c3b58acc8178f8dc8e98d823336489
Annisa Novaguinea <novaguinea.a@gmail.com>
2020-12-28 22:08:49 +0700

    first commit

---
+++ ci_sbd/application/views/Menu/index.php
@@ -0,0 +1,56 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+
+    <div class="row">
+
+        <div class="col-lg-12">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">No.</th>
+                        <th scope="col">Kode Obat</th>
+                        <th scope="col">Nama Obat</th>
+                        <th scope="col">Bentuk Obat</th>
+                        <th scope="col">Tgl Produksi</th>
+                        <th scope="col">Tgl Kadaluarsa</th>
+                        <th scope="col">Harga Satuan</th>
+                        <th scope="col">Edit | Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $no = 1;
+                    $data = $this->db->get('obat');
+                    ?>
+                    <?php
+                    foreach ($data->result_array() as $row) : ?>
+                        <tr>
+                            <td name="tno"><?= $no++; ?></td>
+                            <td><?= $row['KodeObat'] ?></td>
+                            <td><?= $row['NamaObat'] ?></td>
+                            <td><?= $row['BentukObat'] ?></td>
+                            <td><?= $row['TglProduksi'] ?></td>
+                            <td><?= $row['TglKadaluarsa'] ?></td>
+                            <td><?= $row['HargaSatuan'] ?></td>
+                            <td>
+                                <a href="druglist.php?hal=edit&id=<?= $row['id_obat'] ?>" class="btn btn-warning" name="btnedit">
+                                    Edit
+                                </a>
+                                <a href="druglist.php?hal=delete&id=<?= $row['id_obat'] ?>" onclick="return confirm('Are you sure?')" class="btn btn-danger" name="btndelete">
+                                    Delete
+                                </a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
